cfdc7e8cb3784b94342def9410d680aa99cc9eb2
fbseong <fbseong@gmail.com>
2024-06-04 11:37:36 +0900

    ::add Log

--- app/src/main/java/com/selfpro/realies/feature/main/realies/RealiesFragment.kt
+++ app/src/main/java/com/selfpro/realies/feature/main/realies/RealiesFragment.kt
@@ -3,12 +3,13 @@ package com.selfpro.realies.feature.main.realies
 import androidx.fragment.app.viewModels
 import com.selfpro.realies.R
 import com.selfpro.realies.databinding.FragmentRealiesBinding
+import com.selfpro.realies.util.SpLog
 import com.selfpro.realies.util.base.BaseFragment
 
 class RealiesFragment: BaseFragment<FragmentRealiesBinding, RealiesViewModel>(R.layout.fragment_realies) {
     override val viewModel: RealiesViewModel by viewModels()
 
     override fun start() {
-
+        SpLog.d("Start")
     }
 }
