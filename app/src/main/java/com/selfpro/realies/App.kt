7bdb19cc831524364957a0b65093b14bab553d2a
fbseong <fbseong@gmail.com>
2024-06-10 11:01:03 +0900

    :: fetch directory

--- app/src/main/java/com/selfpro/realies/App.kt
+++
@@ -1,6 +0,0 @@
-package com.selfpro.realies
-
-import android.app.Application
-
-class App : Application() {
-}
--- app/src/main/java/com/selfpro/realies/feature/main/realies/RealiesViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.selfpro.realies.feature.main.realies
-
-import com.selfpro.realies.util.base.BaseViewModel
-
-class RealiesViewModel:BaseViewModel() {
-}
